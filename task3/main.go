c2a7d522bd7bc1c5f1631420e7c47dfdeece900f
bumbiyada <bumbiyada@gmail.com>
2022-01-15 23:32:58 +0300

    task3

---
+++ task3/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// First variant
+func Square(n int) (res int) {
+	return (n * n)
+}
+func main() {
+	var WG sync.WaitGroup
+	// making new channel with buffer = 5, it`s important
+	var c = make(chan int, 5)
+	var summ int = 0
+	// starting goroutines in cycle
+	for i := 2; i <= 10; i += 2 {
+		WG.Add(1)
+		go func(i int, WG *sync.WaitGroup, c chan int) {
+			res := Square(i)
+			c <- res
+			WG.Done()
+		}(i, &WG, c)
+
+	}
+	WG.Wait()
+
+	// reading values from channel
+	for i := 0; i < 5; i++ {
+		tmp := <-c
+		summ = summ + tmp
+	}
+	close(c)
+	// Printing result
+	fmt.Println(summ)
+}
