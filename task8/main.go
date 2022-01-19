bae28fd9590508a9061ff2aba168e436aecf1736
bumbiyada <bumbiyada@gmail.com>
2022-01-19 13:36:33 +0300

    completed task8

--- task8/main.go
+++ task8/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func main() {
-	fmt.Println("Hello World")
-	var i int64 = 63
-	fmt.Println(i)
-	fmt.Println(strconv.FormatInt(i, 2)) // print standart number in 101
-	var c int64 = 4
-	fmt.Println(strconv.FormatInt(c, 2)) // print c number in 101
-	a1 := i ^ c
-	fmt.Println(strconv.FormatInt(a1, 2)) // print i ^ c number in 101
-	a2 := i & c
-	fmt.Println(strconv.FormatInt(a2, 2)) // print i & c numnber in 101
+// return 2 ^ n number if n = 1 -> return
+func multiply(n int64) int64 {
+	if (n < 0) || (n > 64) {
+		return -1
+	} else if n == 0 {
+		return 0
+	} else {
+		var res int64 = 1
+		for i := 1; i < int(n); i++ {
+			res = res * 2
+		}
+		return res
+	}
+
 }
+func main() {
+	var i int64 = 64                     //Число, в котором будем менять символ
+	var a int64 = 7                      //Позиция символа который будем менять. от 1 до 63
+	b := multiply(a)                     // Получаем
+	fmt.Printf("start number \t%b\n", i) // print standart number in binary
+	a1 := i ^ b
+	fmt.Printf("result number \t%b\n", a1) // Измененное число
+}
+
+//-64
