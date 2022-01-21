7228a4e7a381a2d842ff2854eb3d650006a8d522
bumbiyada <bumbiyada@gmail.com>
2022-01-21 15:38:09 +0300

    task14

--- task14/main.go
+++ task14/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 //First variant by using fmt.Sprintf
 func type1(obj interface{}) {
@@ -8,8 +11,26 @@ func type1(obj interface{}) {
 	fmt.Println(tmp)
 }
 
+// Second variant by reflect
 func type2(obj interface{}) {
+	tmp := reflect.TypeOf(obj)
+	fmt.Println(tmp)
+}
 
+// Third variant by switch case and Type Assertion
+func type3(obj interface{}) {
+	switch v := obj.(type) {
+	case int:
+		fmt.Println("int", v)
+	case float64:
+		fmt.Println("float64", v)
+	case float32:
+		fmt.Println("float32", v)
+	case string:
+		fmt.Println("string", v)
+	case bool:
+		fmt.Println("bool", v)
+	}
 }
 func main() {
 	var b []interface{}
@@ -18,5 +39,8 @@ func main() {
 
 	for idx, val := range b {
 		fmt.Println(idx, val)
+		type1(val)
+		type2(val)
+		type3(val)
 	}
 }
