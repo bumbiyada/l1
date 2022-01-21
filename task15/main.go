c31ca4fa3ae3125c41d24c8fe3a2cc9a1a589940
bumbiyada <bumbiyada@gmail.com>
2022-01-21 15:55:42 +0300

    task15

---
+++ task15/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// troubles because UTF-8 , ASCII symbols take 1 byte, other unicode symbols may take up to 4 bytes, that`s why
+// take slice of string is not a good idea because we itterate on bytes, not on symbols
+// var justString string
+// func someFunc() {
+// v := createHugeString(1 << 10)
+// justString = v[:100]
+// }
+
+// func main() {
+// someFunc()
+// }
+func main() {
+	//example of crash
+	var a = `превед медвед`
+	var b = a[:5]
+	fmt.Println(b)
+	// correct example, now while using []rune we itterate by symbol, not by bytes
+	var c = []rune(a)
+	d := c[:5]
+	fmt.Printf("%s", string(d))
+}
